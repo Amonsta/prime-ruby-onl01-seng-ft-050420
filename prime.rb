9953a14c0f724d755ced8a40b49e06e8eca20de9
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-17 16:32:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,8 +4,6 @@
 def prime?(number)
   if number <= 1  
     false
-  elsif number == 2 
-    true 
   else 
     (2..number-1).all?{|n| number % n != 0} 
   end 
@@ -19,7 +17,9 @@ end
 if my number is not prime, print FALSE
 use a boolean meathod (?:)
 
-
+elsif number == 2 
+    true 
+    
 (2..number).any?{|n| n % 2 == 0}
 
 To prove whether a number is a prime number, first try dividing it by 2, and see if you get a whole number. If you do, it can't be a prime number. If you don't get a whole number, next try dividing it by prime numbers: 3, 5, 7, 11 (9 is divisible by 3) and so on, always dividing by a prime number
